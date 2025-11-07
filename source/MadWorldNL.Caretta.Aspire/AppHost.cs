3c8e4e5ecd1afa6f54b87c1c445799c16e619130
oveldman <oveldman@gmail.com>
2025-11-07 22:41:55 +0100

    Add `MadWorldNL.Caretta.Controllers.Web` project with Blazor WebAssembly setup.
    Introduced a new project with essential files (`App.razor`, `_Imports.razor`, `Authentication.razor`, `appsettings.json`, `appsettings.Development.json`). Incorporated basic CSS styling and Bootstrap library. Configured authentication and routing.

--- source/MadWorldNL.Caretta.Aspire/AppHost.cs
+++ source/MadWorldNL.Caretta.Aspire/AppHost.cs
@@ -2,9 +2,15 @@ using MadWorldNL.Caretta;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Api>(ProjectDefinitions.Api)
+var api = builder.AddProject<Projects.Api>(ProjectDefinitions.Api)
     .WithUrlForEndpoint("http", c => c.DisplayText = "ApiInsecure")
     .WithUrlForEndpoint("https", c => c.DisplayText = "ApiSecure")
     .WithExternalHttpEndpoints();
+
+builder.AddProject<Projects.Web>(ProjectDefinitions.Web)
+    .WithUrlForEndpoint("http", c => c.DisplayText = "WebInsecure")
+    .WithUrlForEndpoint("https", c => c.DisplayText = "WebSecure")
+    .WithExternalHttpEndpoints()
+    .WaitFor(api);
 
 builder.Build().Run();
--- source/MadWorldNL.Caretta.Aspire/ProjectDefinitions.cs
+++ source/MadWorldNL.Caretta.Aspire/ProjectDefinitions.cs
@@ -3,4 +3,5 @@ namespace MadWorldNL.Caretta;
 public static class ProjectDefinitions
 {
     public const string Api = nameof(Api);
+    public const string Web = nameof(Web);
 }
